6a205300d4bd1b2672eb30432eba3a9312608f34
Divyaansh <divyaansh03anu@gmail.com>
2020-04-29 17:12:07 +0530

    First Class

---
+++ src/main/java/upes/devops/Devops/App.java
@@ -0,0 +1,13 @@
+package upes.devops.Devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/upes/devops/Devops/MyClass.java
@@ -0,0 +1,11 @@
+package upes.devops.Devops;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		System.out.print("This is First Class");
+
+	}
+
+}
